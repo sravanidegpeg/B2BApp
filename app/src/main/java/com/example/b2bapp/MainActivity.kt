609c163c8332a9a8e7889550373e2adfdd0f2393
sravanidegpeg <sravani.veeramachaneni@degpeg.com>
2022-01-20 12:21:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/b2bapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.b2bapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
